7370ee5a5501a990dfd543fdea473302f539f587
BeckoninGshy <405678856@qq.com>
2021-01-26 11:24:46 +0800

    add http response class

---
+++ src/com/yzb/http/HttpResponse.java
@@ -0,0 +1,12 @@
+package com.yzb.http;
+
+import com.yzb.common.Response;
+
+/**
+ * @Description Handle HTTP response
+ * @Date 2021/1/26 上午11:15
+ * @Creater BeckoninGshy
+ */
+public class HttpResponse extends Response {
+
+}
