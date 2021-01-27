fbbffd7e66b6b00d08e915f3cf4c32909e13e0df
BeckoninGshy <405678856@qq.com>
2021-01-27 16:22:31 +0800

    add ConnectorThreadPool

---
+++ src/com/yzb/ConnectorThreadPool.java
@@ -0,0 +1,17 @@
+package com.yzb;
+
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Description handle Connector Thread Pool
+ * @Date 2021/1/27 下午12:07
+ * @Creater BeckoninGshy
+ */
+public class ConnectorThreadPool {
+    private static ThreadPoolExecutor tpe = new ThreadPoolExecutor(20,100,60,TimeUnit.SECONDS,new LinkedBlockingDeque<>(30));
+    public static void run(Runnable runnable){
+        tpe.execute(runnable);
+    }
+}
--- src/com/yzb/Server.java
+++ src/com/yzb/Server.java
@@ -1,7 +1,9 @@
 package com.yzb;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.TimeInterval;
+import cn.hutool.log.LogFactory;
 import com.yzb.exception.ParseHttpRequestException;
-import com.yzb.http.HttpContant;
 import com.yzb.http.HttpProcessor;
 import com.yzb.http.HttpRequest;
 import com.yzb.http.HttpResponse;
@@ -9,22 +11,21 @@ import com.yzb.http.HttpResponse;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.sql.SQLOutput;
-import java.util.Arrays;
 
-public class Server {
+public class Server implements Runnable{
     private volatile static Server serverInstance;
     private int port;
-    private Server(){
+
+    private static ServerSocket serverSocket;
+
+    private Server() {
+
     }
 
-    public static Server getServerInstance(){
-        if(serverInstance == null){
-            synchronized(Server.class){
-                if(serverInstance == null){
+    public static Server getServerInstance() {
+        if (serverInstance == null) {
+            synchronized (Server.class) {
+                if (serverInstance == null) {
                     serverInstance = new Server();
                 }
             }
@@ -32,82 +33,95 @@ public class Server {
         return serverInstance;
     }
 
-    public void setPort(int port){
+    public void setPort(int port) {
         this.port = port;
     }
 
-    public int getPort(){
+    public int getPort() {
         return port;
     }
 
-
     public void start() {
-        ServerSocket serverSocket = null;
+        TimeInterval startTimer = DateUtil.timer();
+        init();
+        LogFactory.get().info("Server startup in {} ms", startTimer.intervalMs());
+    }
+
+    public void init(){
+        Thread service = new Thread(this);
+        service.start();
+        LogFactory.get().info("Initializing service [http-bio-{}]",port);
+    }
+
+    @Override
+    public void run() {
+
         try {
             serverSocket = new ServerSocket(port);
+
+            while (true) {
+
+                Socket socket = serverSocket.accept();
+                Runnable runnable = () -> {
+
+                    try {
+                        HttpRequest httpRequest = new HttpRequest(socket);
+                        HttpResponse httpResponse = new HttpResponse(socket);
+                        HttpProcessor httpProcessor = new HttpProcessor();
+
+                        httpProcessor.execute(socket, httpRequest, httpResponse);
+                    } catch (ParseHttpRequestException e) {
+                        //bad request
+                        e.printStackTrace();
+                    }
+
+                };
+                //add connector to thread pool
+                ConnectorThreadPool.run(runnable);
+
+                //            BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("webapps/form.html")));
+                //            String line = "";
+                //            while((line = bufferedReader.readLine()) != null){
+                //                outputStream.write(line);
+                //                outputStream.write(HttpContant.LINE_TERMINATOR);
+                //            }
+                //            outputStream.flush();
+
+                //            OutputStream outputStream = hs.getOutputStream();
+                //            hs.setHeader(HttpContant.HEADER_CONTENT_TYPE, HttpContant.DEFAULT_CONTENT_TYPE);
+                //            hs.setContentLength("<html><body>hi client!</body></html>".length());
+                //            outputStream.write("<html><body>hi client!</body></html>".getBytes(StandardCharsets.UTF_8));
+                //            outputStream.flush();
+                //            outputStream.close();
+
+                //            FileInputStream bufferedReader = new FileInputStream(new File("webapps/form.html"));
+                //            int size = 0;
+                //            int b = 0;
+                //            while((b = bufferedReader.read()) != -1){
+                //                outputStream.write(b);
+                //                size++;
+                //            }
+                //            hs.setContentLength(size);
+                //            outputStream.flush();
+
+
+                //            FileInputStream bufferedReader = new FileInputStream(new File("webapps/longPdf.pdf"));
+                //            int size = 0;
+                //            int b = 0;
+                //            while((b = bufferedReader.read()) != -1){
+                //                outputStream.write(b);
+                //                size++;
+                //            }
+                //            hs.setContentLength(size);
+                //            hs.setContentType("application/pdf");
+                //            outputStream.flush();
+
+                //            hs.sendRedirect("/hello");
+            }
         } catch (IOException e) {
-            // server init failed!!!
+            // connector failed
             e.printStackTrace();
         }
-        while(true){
-            Socket socket = null;
-            HttpRequest httpRequest = null;
-            HttpResponse httpResponse = null;
-            HttpProcessor httpProcessor = null;
-            try {
-                socket = serverSocket.accept();
-                httpRequest = new HttpRequest(socket);
-                httpResponse = new HttpResponse(socket);
-                httpProcessor = new HttpProcessor();
-                httpProcessor.execute(socket,httpRequest,httpResponse);
-
-            } catch (IOException e) {
-                //accept http request failed!
-                e.printStackTrace();
-            } catch (ParseHttpRequestException e) {
-                //bad http request
-                e.printStackTrace();
-            }
-
-
-            //            BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("webapps/form.html")));
-    //            String line = "";
-    //            while((line = bufferedReader.readLine()) != null){
-    //                outputStream.write(line);
-    //                outputStream.write(HttpContant.LINE_TERMINATOR);
-    //            }
-    //            outputStream.flush();
-
-    //            OutputStream outputStream = hs.getOutputStream();
-    //            hs.setHeader(HttpContant.HEADER_CONTENT_TYPE, HttpContant.DEFAULT_CONTENT_TYPE);
-    //            hs.setContentLength("<html><body>hi client!</body></html>".length());
-    //            outputStream.write("<html><body>hi client!</body></html>".getBytes(StandardCharsets.UTF_8));
-    //            outputStream.flush();
-    //            outputStream.close();
-
-    //            FileInputStream bufferedReader = new FileInputStream(new File("webapps/form.html"));
-    //            int size = 0;
-    //            int b = 0;
-    //            while((b = bufferedReader.read()) != -1){
-    //                outputStream.write(b);
-    //                size++;
-    //            }
-    //            hs.setContentLength(size);
-    //            outputStream.flush();
-
-
-    //            FileInputStream bufferedReader = new FileInputStream(new File("webapps/longPdf.pdf"));
-    //            int size = 0;
-    //            int b = 0;
-    //            while((b = bufferedReader.read()) != -1){
-    //                outputStream.write(b);
-    //                size++;
-    //            }
-    //            hs.setContentLength(size);
-    //            hs.setContentType("application/pdf");
-    //            outputStream.flush();
-
-    //            hs.sendRedirect("/hello");
-        }
     }
+
 }
